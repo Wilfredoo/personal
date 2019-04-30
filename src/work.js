7f9214e7065b79e1f5bbbaaf5ea400cec6d8204e
Wilfredo <casas.farach@gmail.com>
2019-04-30 13:14:33 +0200

    added text to work and other small changes

--- src/work.js
+++ src/work.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Growth } from "./growth.js";
+import { Growth, QA, Design, Copy, Code } from "./workcontent.js";
+
+
 
 export class Work extends React.Component {
   constructor(props) {
@@ -19,6 +21,47 @@ export class Work extends React.Component {
       this.setState({ showGrowth: false });
     }
 
+    handleClickQA() {
+        this.setState({
+          showQA: true,
+        });
+      }
+
+      hideQA() {
+        this.setState({ showQA: false });
+      }
+
+      handleClickCopy() {
+          this.setState({
+            showCopy: true,
+          });
+        }
+
+        hideCopy() {
+          this.setState({ showCopy: false });
+        }
+
+
+        handleClickDesign() {
+            this.setState({
+              showDesign: true,
+            });
+          }
+
+          hideDesign() {
+            this.setState({ showDesign: false });
+          }
+
+          handleClickCode() {
+              this.setState({
+                showCode: true,
+              });
+            }
+
+            hideCode() {
+              this.setState({ showCode: false });
+            }
+
   render() {
     return (
       <div className="modal">
@@ -26,15 +69,42 @@ export class Work extends React.Component {
           <i className="fa fa-close" />
         </button>
           <div id="about">
-            <p>
-              Quality Assurance<br /> Copywrite <br /> Design <br /> Growth <br /> Software Development
-              </p>
               <a onClick={e => this.handleClickGrowth()}  href="#">
             Growth
           </a>
+          <br />
+
+          <a onClick={e => this.handleClickQA()}  href="#">
+        QA
+      </a>
+      <br />
+
+      <a onClick={e => this.handleClickDesign()}  href="#">
+    Design
+  </a>
+  <br />
+
+  <a onClick={e => this.handleClickCopy()}  href="#">
+Copy
+</a>
+<br />
+
+<a onClick={e => this.handleClickCode()}  href="#">
+Code
+</a>
 
           {this.state.showGrowth && (
                     <Growth  close={e => this.hideGrowth()  } /> )}
+                    {this.state.showQA && (
+                              <QA  close={e => this.hideQA()  } /> )}
+                              {this.state.showCopy && (
+                                        <Copy  close={e => this.hideCopy()  } /> )}
+                                        {this.state.showDesign && (
+                                                  <Design  close={e => this.hideDesign()  } /> )}
+                                                  {this.state.showCode && (
+                                                            <Code  close={e => this.hideCode()  } /> )}
+
+
       </div>
       </div>
     );
---
+++ src/workcontent.js
@@ -0,0 +1,184 @@
+import React from "react";
+
+export class Growth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <button onClick={this.props.close} className="btn">
+          <i className="fa fa-close" />
+        </button>
+          <div className="workArea">
+            <p>
+
+            <h4>Jobdirecto</h4>
+JobDirecto grew quickly, and up to date thousands of people have visited to find jobs and dozens of restaurants have used it to find staff. Not a single euro was ever used in advertisement. Lots of work in facebook groups and email marketing was necessary though.
+
+<h4>Colibri</h4>
+While Jon was accountable to build our dating app Colibri, I was accountable to get the users. At first I started going to open mic comedy shows, doing dating jokes on stage and inviting people in for the app. This was fun (for me) but inefficient. Then I went to other dating apps and invited people to test the app. This worked better and got around 150 people to sign in for the app.
+
+<h4>ApruebaEVA</h4>
+How I got hundreds of college students for my finance school apruebaEVA back in old good Peru? Lots of emails and a solid facebook page. Nothing fancy, but it did the work and payed the bills.
+
+
+References:
+Daniel Riaz - danriaz@gmail.com
+
+            </p>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+export class QA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  render() {
+    return (
+      <div className="modal">
+        <button onClick={this.props.close} className="btn">
+          <i className="fa fa-close" />
+        </button>
+          <div className="workArea">
+            <p>
+            QA
+
+            Branching Minds
+            My story with QA start in 2016, when I got an internship in a startup in NYC called Branching Minds. The CTO was also the QA tester, and so taught me how to do QA so he could focus on the programming part. Here I learned the in and outs of manual testing and then I ended up rebuilding the QA testing document.
+
+
+
+            Dog Parker
+            Later I went to Dog Parker. They didnt have any QA protocol in place, so for my first 2 months in the company and with help of the COO I worked every day in creating a QA procedure document for manual testing which tested the dog house, mobile app and admin site. Then I was responsible to manual test it using this doc about once a week.
+
+
+
+            Automated testing, uhh
+            I was lucky enough to befriend and work with Jonathan Poischen, who has a lot of experience as a software engineer, and who in his spare time taught me the fundamentals of QA automation using Ruby and Capybara. This was quite fun and I’d like to improve in this.
+
+
+            References:
+            Todd Schechter - todd@dogparker.com
+            William Farrell - will@branchingminds.com
+            Jonathan Poischen - jonpoischen@gmail.com
+            </p>
+          </div>
+      </div>
+    );
+  }
+}
+
+export class Copy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <button onClick={this.props.close} className="btn">
+          <i className="fa fa-close" />
+        </button>
+          <div className="workArea">
+            <p>
+            COPYWRITE
+            This is my strongest area, as I have written copy for landing pages, facebook pages and lots, lots, lots of marketing emails. I got my two jobs in NYC through a cold email and this single job post got me job interviews with two companies in Berlin (disclaimer: I didn't end come to an agreement with any of them). When it comes to creative and strategic writing, I know my ship.
+
+            References:
+            Elliot Schwarz - elliot@elliot.com
+
+            </p>
+          </div>
+      </div>
+    );
+  }
+}
+
+export class Code extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <button onClick={this.props.close} className="btn">
+          <i className="fa fa-close" />
+        </button>
+          <div className="workArea">
+            <p>
+            SOFTWARE DEVELOPMENT
+
+  JobDirecto
+
+  Colibri waiting list
+
+  And well, this site :)
+
+
+  I kept things minimal for clarity purposes, if u have questions then lets have an interview?
+
+  References:
+
+  matt@fewer.com
+  ivana@spiced.com
+  kristian@hamilton.com
+  jon@poischen.com
+
+
+            </p>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+export class Design extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <button onClick={this.props.close} className="btn">
+          <i className="fa fa-close" />
+        </button>
+          <div className="workArea">
+            <p>
+
+
+            DESIGN
+
+            Guantes la Pepa
+            I guess the first thing I ever designed were these longboarding gloves for Guantes la Pepa. It started as this and through lots of prototyping and testing it became this.
+
+            Colibri waiting list
+            One of my most recent projects was to design this waiting list to get people to sign in for our dating app Colibri. It wasnt easy but it was certainly fun!
+
+            JobDirecto
+            I designed a complex prototype that people didnt use. It was fancy but it was crap. Then I designed this super simplistic job board that people loved. Oh, life...
+
+            And of course... this site. Hopefully you dont find it so crappy!
+
+
+            </p>
+          </div>
+      </div>
+    );
+  }
+}
