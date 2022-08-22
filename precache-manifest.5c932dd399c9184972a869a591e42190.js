6827267ecce4c64d8e33d244433ee9b478fbe6e8
wilfredoo <cacao_de_verdad@protonmail.com>
2022-08-22 17:11:10 +0200

    Updates

---
+++ precache-manifest.5c932dd399c9184972a869a591e42190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd03e21cbfff75fec179738706e2786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb0c935f888fa53d759",
+    "url": "/static/css/main.d48df744.chunk.css"
+  },
+  {
+    "revision": "1aae16dea70a4c351844",
+    "url": "/static/js/2.2eecb307.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2eecb307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb0c935f888fa53d759",
+    "url": "/static/js/main.cd3b5c3c.chunk.js"
+  },
+  {
+    "revision": "aabfb1328a72ee1ec5de",
+    "url": "/static/js/runtime-main.1f095843.js"
+  }
+]);
